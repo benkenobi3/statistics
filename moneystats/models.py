a18d26d97c201766b0dfddd74e8667c5a425738a
Max <32428314+benkenobi3@users.noreply.github.com>
2020-05-19 22:51:34 +0300

    create models: Check, Category, Expense

--- moneystats/models.py
+++ moneystats/models.py
@@ -1,2 +1,21 @@
 from django.db import models
 
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=200)
+
+
+class Check(models.Model):
+
+    date = models.DateTimeField()
+
+
+class Expense(models.Model):
+
+    name = models.CharField(max_length=200)
+    price = models.IntegerField(default=0)
+    date = models.DateTimeField()
+
+    category = models.ForeignKey(Category, related_name='expenses', on_delete=models.CASCADE)
+    cash_check = models.ForeignKey(Check, related_name='positions', on_delete=models.CASCADE, null=True)
