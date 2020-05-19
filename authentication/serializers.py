df95ae6c92a67af5fd7430d59bd7e6db1ec11b13
Max <32428314+benkenobi3@users.noreply.github.com>
2020-05-19 22:43:33 +0300

    minor fix some shit

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
-from rest_framework import serializers, status
-from rest_framework.response import Response
+from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
