b2302f8c1ed5d94f66fce9c35483d23a51f72863
Max <32428314+benkenobi3@users.noreply.github.com>
2020-05-27 18:34:06 +0300

    add router in urls.py

--- moneystats/urls.py
+++ moneystats/urls.py
@@ -1,9 +1,13 @@
 from .views import *
-from django.conf.urls import url
+from django.conf.urls import url, include
+from rest_framework import routers
 
+router = routers.DefaultRouter()
+router.register(r'detail/', ExpenseDetailAPIView)
 
 urlpatterns = [
     url(r'^diagram/$', diagram_data),
+    url(r'^expenses/', include(router.urls)),
     url(r'expenses/all', ExpenseListAPIView.as_view()),
     url(r'expenses/list', ExpensesUserListAPIVIew.as_view()),
     url(r'expenses/create', ExpenseCreateAPIView.as_view()),
